69f72371a9830ab92242cd7539a41e9ae51b70e6
Gwidon <gwidon.szczepankiewicz@student.put.poznan.pl>
2025-05-27 07:50:50 +0200

    Train a RandomForestClassifier model and make predictions

--- src/predict.py
+++ src/predict.py
@@ -1,25 +1,77 @@
 import mlflow
-import mlflow.pyfunc
+import mlflow.sklearn
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Load the dataset with player data
 df = pd.read_csv('../data/league_leaders_2024-25.csv')
-X_test = features = df.drop(columns=['PLAYER_ID', 'RANK', 'PLAYER', 'TEAM_ID', 'TEAM'])
-print(X_test.head())
+df = df[(df['GP'] >= 65)]
+X_latest = features = df.drop(columns=['PLAYER_ID', 'RANK', 'PLAYER', 'TEAM_ID', 'TEAM'])
+#X_latest = X_latest[['EFF','PTS','FTA', 'FGM','FTM','FGA','TOV','MIN','DREB']]
+
 
 
 # Load the model back for predictions as a generic Python Function model
 # Replace 'model_uri' with the actual path or URI to your MLflow model
-model_uri = "../mlartifacts/524645951732338284/bfd2812fe4f24f198ee64a5a17093541/artifacts/nba_model" 
-loaded_model = mlflow.pyfunc.load_model(model_uri)
+model_uri = "../mlartifacts/524645951732338284/1cbf3797b16847d4aa50c4d039d7392a/artifacts/nba_model" 
+loaded_model = mlflow.sklearn.load_model(model_uri)
 
-predictions = loaded_model.predict(X_test)
+# Załóżmy, że używałeś tych kolumn:
+feature_names = X_latest.columns
+
+# Ważności cech
+importances = loaded_model.feature_importances_
+
+# Stwórz DataFrame
+feature_importance_df = pd.DataFrame({
+    'feature': feature_names,
+    'importance': importances
+}).sort_values(by='importance', ascending=False)
+
+# Wykres słupkowy
+plt.figure(figsize=(10, 6))
+plt.barh(feature_importance_df['feature'][:15][::-1], feature_importance_df['importance'][:15][::-1])
+plt.xlabel("Ważność cechy")
+plt.title("Top 15 najważniejszych cech (RandomForestClassifier)")
+plt.tight_layout()
+plt.show()
 
 
+# Predict probabilities
+probs = loaded_model.predict_proba(X_latest)[:, 1]
 
-result = pd.DataFrame({
-    "PLAYER": df["PLAYER"],
-    "predicted_class": predictions
-})
+# Dodaj je do ramki
+latest_season = df.copy()
+latest_season['all_nba_prob'] = probs
 
-result.to_csv('../data/predictions_all_nba_teams.csv', index=False)
+print(latest_season[['PLAYER', 'all_nba_prob']])
+
+# Sortuj i wybierz Top 15
+top15 = latest_season.sort_values(by='all_nba_prob', ascending=False).head(15)
+
+# Przypisz teamy
+top15['pred_team'] = [1]*5 + [2]*5 + [3]*5
+
+# Pokaż wynik
+print(top15[['PLAYER', 'all_nba_prob', 'pred_team']])
+
+
+# result = pd.DataFrame({
+#     "PLAYER": df["PLAYER"],
+#     "predicted_class": predictions
+# })
+
+# result.to_csv('../data/predictions_all_nba_teams.csv', index=False)
+
+# # Function to generate JSON file with NBA teams
+# def generate_all_nba_json(first_team, second_team, third_team, first_rookie_team, second_rookie_team, filename):
+#     data = {
+#         "first all-nba team": first_team,
+#         "second all-nba team": second_team,
+#         "third all-nba team": third_team,
+#         "first rookie all-nba team": first_rookie_team,
+#         "second rookie all-nba team": second_rookie_team
+#     }
+
+#     with open(filename, 'w') as json_file:
+#         json.dump(data, json_file, indent=2)
--- src/train_all_nba_teams.py
+++ src/train_all_nba_teams.py
@@ -3,39 +3,68 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from xgboost import XGBClassifier
-from sklearn.metrics import classification_report, accuracy_score
+from sklearn.metrics import classification_report, accuracy_score, f1_score
 import matplotlib.pyplot as plt
 import mlflow
 import mlflow.xgboost
 from mlflow.models.signature import infer_signature
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestClassifier
+import os
+from sklearn.model_selection import GridSearchCV
 
 mlflow.set_tracking_uri(uri="http://127.0.0.1:8080")
 
 # Load the dataset
-data = pd.read_csv('../data/final_dataset_all_nba_teams.csv')
+data = pd.read_csv('../data/final_dataset_all_nba_teams.csv', index_col=False)
+
+# Create a new binary column if the player is in the All-NBA team
+data['all_nba'] = data['Tm'].apply(lambda x: 0 if x == '0' else 1)
+
+data = data[(data['GP'] >= 65)]
+
 
 # Choose the features and labels
-features = data.drop(columns=['PLAYER_ID', 'RANK', 'PLAYER', 'TEAM_ID', 'TEAM', 'SEASON'])
-labels = data['Label']
-
-# Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42, stratify=labels)
-
+features = data.drop(columns=['PLAYER_ID', 'RANK', 'PLAYER', 'TEAM_ID', 'TEAM', 'SEASON', 'Tm', 'all_nba'])
+#features = features[['EFF','PTS','FTA', 'FGM','FTM','FGA','TOV','MIN','DREB']]
+labels = data['all_nba']
 
 # Feature scaling
 scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
+X_scaled = scaler.fit_transform(features)
+
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, labels, test_size=0.3, random_state=42, stratify=labels)
+
 
 # Log the model parameters and metrics with MLflow
-params = {"use_label_encoder": False, "eval_metric": 'logloss'}
+params = {'class_weight': 'balanced', 'n_estimators': 100, 'random_state': 42}
 
-# Train the XGBoost model
-model = XGBClassifier(**params)
-model.fit(X_train, y_train)
+# Train the RandomForestClassifier model
+model = RandomForestClassifier(**params)
+
+param_grid = {
+    'n_estimators': [100, 200, 500],
+    'max_depth': [None, 10, 20],
+    'min_samples_split': [2, 5, 10],
+    'min_samples_leaf': [1, 2, 4]
+}
+
+grid_search = GridSearchCV(
+    estimator=model,
+    param_grid=param_grid,
+    scoring='f1',  # lub 'f1'
+    cv=5,
+    n_jobs=-1,
+    verbose=1
+)
+
+
+grid_search.fit(X_train, y_train)
+best_model = grid_search.best_estimator_
 
 # Predykcja i ocena modelu
-y_pred = model.predict(X_test)
+y_pred = best_model.predict(X_test)
 print(classification_report(y_test, y_pred))
 print(f"Accuracy: {accuracy_score(y_test, y_pred)}")
 
@@ -45,22 +74,27 @@ mlflow.set_experiment("Training NBA Teams Model")
 # Start an MLflow run
 with mlflow.start_run():
     # Log the hyperparameters
-    mlflow.log_params(params)
+    mlflow.log_params(grid_search.best_params_)
 
     # Log the loss metric
-    mlflow.log_metric("accuracy", accuracy_score(y_test, y_pred))
+    mlflow.log_metric("f1_score", grid_search.best_score_)
 
     # Set a tag that we can use to remind ourselves what this run was for
-    mlflow.set_tag("Training Info", "Basic XGBoost model for NBA teams classification")
+    mlflow.set_tag("Training Info", "Changed number of parameters")
+    
+
+
+    dataset_path = "../data/final_dataset_all_nba_teams.csv"
+    mlflow.log_artifact(dataset_path, artifact_path="dataset")
 
     # Infer the model signature
-    signature = infer_signature(X_train, model.predict(X_train))
+    signature = infer_signature(X_train, best_model.predict(X_train))
 
     # Log the model
     model_info = mlflow.sklearn.log_model(
-        sk_model=model,
+        sk_model = best_model,
         artifact_path="nba_model",
         signature=signature,
         input_example=X_train,
-        registered_model_name="tracking-quickstart",
+        registered_model_name="RandomForestClassifier",
     )
